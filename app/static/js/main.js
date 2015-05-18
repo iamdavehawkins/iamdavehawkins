165d0449c9a22f22db1f49c46afafd7d0a565dcf
iamdavehawkins <iamdavehawkins@gmail.com>
2015-05-17 22:20:40 -0400

    active nav, all templates, LESS

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function() {
+	$('ul.nav li a').click(function() {	
+		$('ul.nav li a').filter(function() {
+    		return this.href == url;
+		}).parent().addClass('active');
+	})
+})
--- app/views.py
+++ app/views.py
@@ -4,9 +4,25 @@ from app.models import Bike
 
 @app.route('/')
 @app.route('/index')
+@app.route('/home')
 def index():
-    bikes = Bike.query.all()
-    return render_template('base.html', bikes=bikes)
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/bikes')
+def bikes():
+    return render_template('bikes.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
