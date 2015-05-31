fd3328b45d87abd76125d79669783aab9dd2f671
iamdavehawkins <iamdavehawkins@gmail.com>
2015-05-31 17:35:04 -0400

    moved config

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from contextlib import closing
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 from app import views
--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-DEBUG = False
-SQLALCHEMY_ECHO = False
-SQLALCHEMY_DATABASE_URI = os.environ('DATABASE_URL')
