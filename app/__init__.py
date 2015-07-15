22f7e7d220303581cbc284ad739edbfb99d79eb9
iamdavehawkins <iamdavehawkins@gmail.com>
2015-07-14 23:23:11 -0400

    static content to s3

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_s3 import FlaskS3
 from flask.ext.sqlalchemy import SQLAlchemy
 from contextlib import closing
 
 app = Flask(__name__)
+app.config['S3_BUCKET_NAME'] = 'iamdavehawkins'
+s3 = FlaskS3(app)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 from app import views
